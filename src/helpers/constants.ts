818ba4075a7a100c6d0c024fd36bdba6457aea65
Maxim Nesterenko <75580691+vira-hub@users.noreply.github.com>
2023-01-04 21:22:21 +0200

    feat: changed constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,4 @@
 export const CHAIN_ID_MAIN = 1;
 export const CHAIN_ID_GOERLI = 5;
 
-export const WEB3_JAR_FACTORY_ADDRESS = "";
-export const WEB3_JAR_ADDRESS = "";
+export const WEB3_JAR_FACTORY_ADDRESS = "0x5fa15bb817FAFC26E0d1e72BB4f29F32ea50b066"; // goerli
