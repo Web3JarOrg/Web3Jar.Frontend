d192c6c2f81c778ed0cad4f88ffe313af27e3ace
Maxim Nesterenko <75580691+vira-hub@users.noreply.github.com>
2023-01-09 23:43:16 +0200

    feat: searchbar impl

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,55 +1,30 @@
-import { ethers } from "ethers";
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 
-import styles from "./style.module.scss";
-
-import Form from "../Form";
 import Input from "../Input";
 
-const SearchBar: FC = () => {
-    const [jarAddress, setJarAddress] = useState<string>("");
-    const [errorText, setErrorText] = useState<string>();
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-        setJarAddress(e.target.value);
+type SearchBarProps = {
+  setSearchableValue: React.Dispatch<React.SetStateAction<string>>;
+  searchableValue: string;
+};
 
-    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!jarAddress) {
-            setErrorText("Error: Enter jar address first");
-            return;
-        }
-        const isValidAddress = ethers.utils.isAddress(jarAddress);
-        !isValidAddress
-            ? setErrorText("Error: Invaid contract address")
-            : setErrorText("");
+const SearchBar: FC<SearchBarProps> = ({
+  setSearchableValue,
+  searchableValue,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchableValue(e.target.value);
+  };
 
-        setJarAddress("");
-    };
-
-    return (
-        <>
-            <Form handleSubmit={handleSubmit} errorText={errorText} buttonText={'Find jar'}>
-                <Input
-                    type="text"
-                    placeholder="Enter jar address..."
-                    value={jarAddress}
-                    onChange={handleChange}
-                />
-            </Form>
-            {/*<form onSubmit={handleSubmit} className={styles.inputContainer}>
-                <input
-                    type={"text"}
-                    placeholder="Enter jar address..."
-                    value={jarAddress}
-                    onChange={handleChange}
-                />
-                <button className="walletButton" type="submit">
-                    Find jar
-                </button>
-            </form>
-            {errorText && <p className={styles.error}>{errorText}</p>}*/}
-        </>
-    );
+  return (
+    <>
+      <Input
+        type="text"
+        placeholder="Enter jar address..."
+        value={searchableValue}
+        onChange={handleChange}
+      />
+    </>
+  );
 };
 
 export default SearchBar;
