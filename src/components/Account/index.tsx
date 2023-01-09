78664d09becf3c44acb6200205acab3107e7f40b
MarfleXX <97696544+MarfleXX@users.noreply.github.com>
2023-01-09 23:38:46 +0200

    refactor: updated account component

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -4,35 +4,41 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import styles from "./style.module.scss";
 
 import useWallet from "../../hooks/useWallet";
+import { shortenAddr } from "../../utils/shortenAddress";
 
 interface IAccountProps {
-    account: string;
-};
+  account: string;
+}
 
 const Account: FC<IAccountProps> = ({ account }) => {
-    const { disconnectWallet } = useWallet();
-    const [isOpened, setIsOpened] = useState<boolean>(false);
+  const { disconnectWallet } = useWallet();
+  const [isOpened, setIsOpened] = useState<boolean>(false);
 
-    const handleClick = () => setIsOpened((prevState) => !prevState);
+  const handleClick = () => setIsOpened((prevState) => !prevState);
+  const shortenedAddress = shortenAddr(account);
 
-    return (
-        <div
-            style={isOpened ? { borderBottom: 0, borderRadius: "8px 8px 0 0" } : {}}
-            className={styles.account}
-        >
-            <div className={styles.wrapper}>
-                <p>{account.substring(0, 6) + "..." + account.substring(37)}</p>
-                <MdOutlineKeyboardArrowDown className={styles.icon} size={"25px"} onClick={handleClick}/>
-            </div>
-            {isOpened && (
-                <div className={styles.dropdown}>
-                    <button className="walletButton" onClick={disconnectWallet}>
-                        Disconnect
-                    </button>
-                </div>
-            )}
+  return (
+    <div
+      style={isOpened ? { borderBottom: 0, borderRadius: "8px 8px 0 0" } : {}}
+      className={styles.account}
+    >
+      <div className={styles.wrapper}>
+        <p>{shortenedAddress}</p>
+        <MdOutlineKeyboardArrowDown
+          className={styles.icon}
+          size={"25px"}
+          onClick={handleClick}
+        />
+      </div>
+      {isOpened && (
+        <div className={styles.dropdown}>
+          <button className="walletButton" onClick={disconnectWallet}>
+            Disconnect
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Account;
